da83b530e6263276f409c32f10a4ab0d91897f77
Juan <unify.app.and@gmail.com>
2023-11-18 20:13:58 +0100

    Project creation

---
+++ This is Firebase/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  This is Firebase
+//
+//  Created by Juan Sancho García on 18/11/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
